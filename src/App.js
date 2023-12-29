9b0f3c84249faa91f0c88e9163b4da87195bab40
arvind <arvindsharan46@gmail.com>
2023-12-29 16:29:05 +0530

    done with image slider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import Accordion from "./components/accordion";
+import ImageSlider from "./components/image-slider";
 import RandomColor from "./components/random-color";
-import StarRating from "./components/star-rating";
 
 function App() {
   return (
     <div className="App">
+      {/* accordion component */}
       {/* <Accordion /> */}
+      {/* random color component */}
       {/* <RandomColor /> */}
-      <StarRating noOfStars={10} />
+      {/* star rating component */}
+      {/* <StarRating noOfStars={10} /> */}
+      {/* image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
